589cb65d9db7135b0665e96d284d1a899fd2592f
Mustafa Buda <mustafabuda@gmail.com>
2016-02-09 20:31:23 +0200

    Fixed error with slug url for detail view

--- src/localfriend/urls.py
+++ src/localfriend/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^detail/(?P<object_id>\d+)/$', 'tours.views.detail_view', name='detail_view'),
-    url(r'^detail/(?P<slug>[\W-]+)/$', 'tours.views.detail_slug_view', name='detail_slug_view'),
+    url(r'^detail/(?P<slug>[\w-]+)/$', 'tours.views.detail_slug_view', name='detail_slug_view'),
     url(r'^list/$', 'tours.views.list_view', name='list_view'),
 ]
