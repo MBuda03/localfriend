8bae906bb4aeede2988422dbfceef9698e3de806
Mustafa Buda <mustafabuda@gmail.com>
2016-02-09 18:18:46 +0200

    Max length added to tour title as 140 chars

--- src/tours/models.py
+++ src/tours/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Tour(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=140)
 
     def __unicode__(self):
         return self.title
