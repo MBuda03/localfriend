5b806326aece3ef5cafab83d2ffe27c5a4c3ebec
Mustafa Buda <mustafabuda@gmail.com>
2016-02-09 20:00:35 +0200

    detail view with tour id to urls added

--- src/localfriend/urls.py
+++ src/localfriend/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^detail/$', 'tours.views.detail_view', name='detail_view'),
+    url(r'^detail/(?P<object_id>\d+)$', 'tours.views.detail_view', name='detail_view'),
     url(r'^list/$', 'tours.views.list_view', name='list_view'),
 ]
--- src/tours/views.py
+++ src/tours/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
+from tours.models import Tour
 
 # Create your views here.
 
 # Returns 1 tour with details
-def detail_view(request):
-    print request
+def detail_view(request, object_id=None):
+    tour = get_object_or_404(Tour, id=object_id)
     template = "detail_view.html"
-    context = {}
+    context = {
+        "object": tour
+        }
     return render(request, template, context)
+
 
 # Returns list of tours
 def list_view(request):
