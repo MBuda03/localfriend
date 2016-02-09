4239fa0fd33dbdd323c37f6b9feaf1213ad6d5ca
Mustafa Buda <mustafabuda@gmail.com>
2016-02-09 19:18:58 +0200

    urls for list and detail view of tours added

--- src/localfriend/urls.py
+++ src/localfriend/urls.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^detail/$', 'tours.views.detail_view', name='detail_view'),
+    url(r'^list/$', 'tours.views.list_view', name='list_view'),
 ]
