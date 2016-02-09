b2c12d67d5f7ccfbb72afe0e9d56010c6e3b914d
Mustafa Buda <mustafabuda@gmail.com>
2016-02-09 19:12:45 +0200

    Views for list of tours and 1 specific tour added

--- src/tours/views.py
+++ src/tours/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Returns 1 tour with details
+def detail_view(request):
+    print request
+    template = "detail_view.html"
+    context = {}
+    return render(request, template, context)
+
+# Returns list of tours
+def list_view(request):
+    print request
+    template = "list_view.html"
+    context = {}
+    return render(request, template, context)
