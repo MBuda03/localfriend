c424eb82ed0f590eedd10cd31923ab66eff2b5df
Mustafa Buda <mustafabuda@gmail.com>
2016-02-10 00:16:46 +0200

    Better way to create forms created

--- src/tours/forms.py
+++ src/tours/forms.py
@@ -1,9 +1,52 @@
 from django import forms
 
+from tours.models import Tour
+
+
+PUBLISHED_CHOICES = (
+    ('publish', "Publish"),
+    ('draft', "Draft"),
+    )
+
 class TourAddForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={
+            "class" : "title-class",
+            "placeholder" : "Title",
+        }))
+
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={
+            "class" : "description-class",
+            "placeholder" : "Description",
+        }))
+
     price = forms.DecimalField()
+    publish = forms.ChoiceField(widget=forms.RadioSelect, choices=PUBLISHED_CHOICES)
+
+    # Form Validations Here
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+        return price
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if len(title) > 140:
+            raise forms.ValidationError("Title cannot be greater than 140 characters long.")
+        elif len(title) < 5:
+            raise forms.ValidationError("Title cannot be smaller than 5 characters long.")
+        else:
+            return title
+
+
+class TourModelForm(forms.ModelForm):
+    class Meta:
+        model = Tour
+        fields = [
+            "title",
+            "description",
+            "price",
+        ]
 
     # Form Validations Here
     def clean_price(self):
--- src/tours/views.py
+++ src/tours/views.py
@@ -1,13 +1,20 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
-from tours.forms import TourAddForm
+from tours.forms import TourAddForm, TourModelForm
 from tours.models import Tour
+from django.utils.text import slugify
 
 # Create your views here.
 
 def create_view(request):
-    form = TourAddForm(request.POST or None)
+    form = TourModelForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.slug = slugify(instance.title)
+        instance.save()
+
+    """
     if form.is_valid():
         data = form.cleaned_data
         title = data.get("title")
@@ -18,7 +25,9 @@ def create_view(request):
         new_obj.description = description
         new_obj.price = price
         new_obj.save()
-        
+    """
+
+
     template = "create_view.html"
     context = {
         "form":form,
