54545f36c6fc89cbc3ea044c90bf09a0d7325009
Mustafa Buda <mustafabuda@gmail.com>
2016-02-18 19:53:41 +0200

    Simplified  urls

--- src/localfriend/urls.py
+++ src/localfriend/urls.py
@@ -17,13 +17,10 @@ urlpatterns = [
     url(r'^detail/(?P<object_id>\d+)/$', 'tours.views.detail_view', name='detail_view'),
     url(r'^detail/(?P<slug>[\w-]+)/$', 'tours.views.detail_slug_view', name='detail_slug_view'),
     url(r'^list/$', 'tours.views.list_view', name='list_view'),
-    url(r'^tours/$', TourListView.as_view(), name='tour_list_view'),
-    url(r'^tours/add/$', TourCreateView.as_view(), name='tour_create_view'),
 
-    url(r'^tours/(?P<pk>\d+)/$', TourDetailView.as_view(), name='tour_detail_view'),
-    url(r'^tours/(?P<slug>[\w-]+)/$', TourDetailView.as_view(), name='tour_detail_slug_view'),
-    url(r'^tours/(?P<pk>\d+)/edit/$', TourUpdateView.as_view(), name='tour_update_view'),
-    url(r'^tours/(?P<slug>[\w-]+)/edit/$', TourUpdateView.as_view(), name='tour_update_slug_view'),
+    url(r'^tours/',include("products.urls") ),
+
+    
 
 
 
---
+++ src/tours/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from .views import (
+                TourListView,
+                TourDetailView,
+                TourCreateView,
+                TourUpdateView,
+                )
+
+
+urlpatterns = [
+
+    url(r'^$', TourListView.as_view(), name='tour_list_view'),
+    url(r'^add/$', TourCreateView.as_view(), name='tour_create_view'),
+    url(r'^(?P<pk>\d+)/$', TourDetailView.as_view(), name='tour_detail_view'),
+    url(r'^(?P<slug>[\w-]+)/$', TourDetailView.as_view(), name='tour_detail_slug_view'),
+    url(r'^(?P<pk>\d+)/edit/$', TourUpdateView.as_view(), name='tour_update_view'),
+    url(r'^(?P<slug>[\w-]+)/edit/$', TourUpdateView.as_view(), name='tour_update_slug_view'),
+
+
+
+
+]
