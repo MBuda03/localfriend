a2c4c30200bb7c07cf8de6d67494c308ff8d5280
Mustafa Buda <mustafabuda@gmail.com>
2016-02-18 19:56:59 +0200

    simplified urls

--- src/tours/urls.py
+++ src/tours/urls.py
@@ -10,12 +10,12 @@ from .views import (
 
 urlpatterns = [
 
-    url(r'^$', TourListView.as_view(), name='tour_list_view'),
-    url(r'^add/$', TourCreateView.as_view(), name='tour_create_view'),
-    url(r'^(?P<pk>\d+)/$', TourDetailView.as_view(), name='tour_detail_view'),
-    url(r'^(?P<slug>[\w-]+)/$', TourDetailView.as_view(), name='tour_detail_slug_view'),
-    url(r'^(?P<pk>\d+)/edit/$', TourUpdateView.as_view(), name='tour_update_view'),
-    url(r'^(?P<slug>[\w-]+)/edit/$', TourUpdateView.as_view(), name='tour_update_slug_view'),
+    url(r'^$', TourListView.as_view(), name='list'),
+    url(r'^add/$', TourCreateView.as_view(), name='create'),
+    url(r'^(?P<pk>\d+)/$', TourDetailView.as_view(), name='detail'),
+    url(r'^(?P<slug>[\w-]+)/$', TourDetailView.as_view(), name='detail_slug'),
+    url(r'^(?P<pk>\d+)/edit/$', TourUpdateView.as_view(), name='update'),
+    url(r'^(?P<slug>[\w-]+)/edit/$', TourUpdateView.as_view(), name='update_slug'),
 
 
 
