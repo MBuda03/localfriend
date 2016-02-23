e73ccee7f58570610cb673ea59d268a560b1b37e
Mustafa Buda <mustafabuda@gmail.com>
2016-02-23 19:43:49 +0200

    template for index created

--- src/tours/urls.py
+++ src/tours/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from .views import (
                 TourListView,
@@ -10,12 +11,23 @@ from .views import (
 
 urlpatterns = [
 
+
+
+
     url(r'^$', TourListView.as_view(), name='list'),
+
+
+    #url(r'^test/$', TemplateView.as_view(template_name='/index.html'), name='test_index'),
+
+
     url(r'^add/$', TourCreateView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/$', TourDetailView.as_view(), name='detail'),
     url(r'^(?P<slug>[\w-]+)/$', TourDetailView.as_view(), name='detail_slug'),
     url(r'^(?P<pk>\d+)/edit/$', TourUpdateView.as_view(), name='update'),
     url(r'^(?P<slug>[\w-]+)/edit/$', TourUpdateView.as_view(), name='update_slug'),
+
+
+
 
 
 
